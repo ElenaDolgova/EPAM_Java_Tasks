88861510fd2ad96453e5d11f769dc7c40b0c131c
Elena Dolgova <levinforce@gmail.com>
2016-10-07 16:30:24 +0300

    1.3 was added

---
+++ OneThree/src/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void  main(String[] args ){
+        Scanner s= new Scanner(System.in);
+        System.out.println("Input h ");
+        double h=s.nextDouble();
+        System.out.println("Input a ");
+        double a=s.nextDouble();
+        System.out.println("Input b ");
+        double b=s.nextDouble();
+        out1(a,b,h);
+
+    }
+    private static void out1(double a, double b, double h){
+        for (double i=a;i<b;i=i+h){
+            System.out.print(i+" ");
+            System.out.println(function(i));
+        }
+    }
+    private static double function (double x){
+        return Math.tan(2*x)-3;
+    }
+}
