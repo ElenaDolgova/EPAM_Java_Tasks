263c0aa52b5075c43d7b36f3842011b35d7025dd
Elena Dolgova <levinforce@gmail.com>
2016-10-08 18:14:59 +0300

    08.10.16 18:14

---
+++ src/javase01/t05/main/Main.java
@@ -0,0 +1,27 @@
+package javase01.t05.main;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Input the dimension of the matrix: ");
+        Scanner s= new Scanner(System.in);
+        //int n=s.nextInt();
+        matrix(s.nextInt());
+        }
+
+        private static void matrix(int n){
+            int[][] mass=new int[n][n];
+            for (int i=0;i<n;i++) {
+
+                for(int j=0;j<n;j++){
+                    if((i==j)|((i+j)==(n-1))){ // проврка на единички, стоящие на дагонали
+                        mass[i][j]=1;
+                    }
+                    System.out.print(mass[i][j]);
+                }
+                System.out.println();
+
+            }
+        }
+
+}
