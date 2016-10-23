5940de3bd6f82c11c5e9ba323a3395c4e011af4e
Elena Dolgova <levinforce@gmail.com>
2016-10-24 01:28:20 +0300

    renew

--- src/tests/java/javase03/t02/TestBundle.java
+++ src/tests/java/javase03/t02/TestBundle.java
@@ -4,6 +4,7 @@ import static javase03.t02.Bundle.chooseLanguage;
 
     public class TestBundle {
         public static void main(String[] args) {
+
             chooseLanguage();
         }
     }
