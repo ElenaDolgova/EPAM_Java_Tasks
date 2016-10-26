c989fe1ebb1f25626be58157ade7c3ef91b1adf7
Elena Dolgova <levinforce@gmail.com>
2016-10-26 20:35:25 +0300

    Измененены компартаоры

--- src/main/java/javase02/t03/baseThing/setOfBiginner/byPrice.java
+++ src/main/java/javase02/t03/baseThing/setOfBiginner/byPrice.java
@@ -9,17 +9,18 @@ public class byPrice implements Comparator<BaseThing> {
 
         public int compare(BaseThing o1, BaseThing o2) {
 
-            int price1 = o1.getPrice();
-            int price2 = o2.getPrice();
-
-            if (price1 < price2) {
-                return -1;
-            } else if (price1 > price2) {
-                return 1;
-            } else {
-                return 0;
-
-            }
+            Integer price1 = o1.getPrice();
+            Integer price2 = o2.getPrice();
+            return price1.getClass().getName().compareTo(price2.getClass().getName());
+//
+//            if (price1 < price2) {
+//                return -1;
+//            } else if (price1 > price2) {
+//                return 1;
+//            } else {
+//                return 0;
+//
+//            }
 
     }
 }
--- src/main/java/javase02/t03/baseThing/setOfBiginner/byPriceAndByName.java
+++ src/main/java/javase02/t03/baseThing/setOfBiginner/byPriceAndByName.java
@@ -8,19 +8,22 @@ import java.util.Comparator;
 public class byPriceAndByName implements Comparator<BaseThing> {
     public int compare(BaseThing o1, BaseThing o2) {
         int var=o1.getClass().getName().compareTo(o2.getClass().getName());
-        int price1 = o1.getPrice();
-        int price2 = o2.getPrice();
+        Integer price1 = o1.getPrice();
+        Integer price2 = o2.getPrice();
 
         if(var!=0){
             return var;
-        }else if (price1 < price2) {
-            return -1;
-        } else if (price1 > price2) {
-            return 1;
-        } else {
-            return 0;
-
-        }
+        }else
+        return price1.getClass().getName().compareTo(price2.getClass().getName());
+//
+//            if (price1 < price2) {
+//            return -1;
+//        } else if (price1 > price2) {
+//            return 1;
+//        } else {
+//            return 0;
+//
+//        }
 
     }
 }
