dc66e32b03b8474cfffcc7b30563c4b48121b28b
Elena Dolgova <levinforce@gmail.com>
2016-11-06 03:24:17 +0300

    javase04.t04

--- src/tests/java/javase04/t04/SerializationTest.java
+++ src/tests/java/javase04/t04/SerializationTest.java
@@ -1,13 +1,11 @@
-package javase04;
+package javase04.t04;
 
-import javase04.t04.Serialization;
 import org.junit.Test;
 
 public class SerializationTest {
     @Test
     public void serializationTest(){
         String name="D:\\Work\\src\\main\\resources\\films.txt";
-
         Serialization s=new Serialization();
         s.addNewTextFile(name);
         s.readFile();
