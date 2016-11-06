d13674b9e5605549b005d6c7c30ed4cd4311f419
Elena Dolgova <levinforce@gmail.com>
2016-11-06 06:35:59 +0300

    javase05.t01

--- src/main/java/javase05/t01/Catalog.java
+++ src/main/java/javase05/t01/Catalog.java
@@ -1,4 +1,59 @@
 package javase05.t01;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
 public class Catalog {
-}
+    private StringBuilder mainDirectory=new StringBuilder("D:\\Work\\src\\main\\resources\\catalog");
+
+    public void changePath(){
+
+        System.out.println("Choose path:\n0) Exite");
+
+        File[] fList;
+        File F = new File(mainDirectory.toString());
+        fList = F.listFiles();
+
+        Map<Integer,String> map= new HashMap<>();
+        int j=1;
+        for (File i : fList ) {
+            map.put(0,"Exit");
+            map.put(j,i.getName());
+            System.out.println(j+") "+i.getName());
+            j++;
+        }
+        Scanner s = new Scanner(System.in);
+        int l = s.nextInt();
+        //s.close();
+
+        while(l!=0) {
+            map.put(0,"Exit");
+
+               mainDirectory.append("\\"+map.get(l));
+
+            F = new File(mainDirectory.toString());
+           // System.out.println(mainDirectory);
+            fList=F.listFiles();
+            System.out.println("Choose path:\n0) Exite");
+           // System.out.println(fList.toString());
+            j=1;
+            try{
+                for (File i : fList ) {
+                    map.put(j,i.getName());
+                    System.out.println(j+") "+i.getName());
+                    j++;
+                }
+            }catch (NullPointerException e){
+                System.out.println("Catalog is not exist, please select the proposed folder ");
+                mainDirectory.delete(mainDirectory.indexOf("null"),mainDirectory.indexOf("null")+6);
+            }
+
+            l = s.nextInt();
+    }
+
+        }
+    }
+
---
+++ src/tests/java/javase05/t01/CatalogTest.java
@@ -0,0 +1,11 @@
+package javase05.t01;
+
+import org.junit.Test;
+
+public class CatalogTest {
+
+    public static void main(String[] args){
+        Catalog c= new Catalog();
+        c.changePath();
+    }
+}
